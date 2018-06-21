26827df6951a4387db986643d1ea7303402fe522
Julien Gascon-Samson <julien.gascon-samson@ece.ubc.ca>
2018-06-20 18:41:03 -0700

    Initial - un-tested / un-run implementation of the Java side of ExternalClient. TODO: write the JS side as a pub/sub engine.

--- src/Dynamoth/Core/ExternalClient/ExternalClient.java
+++ src/Dynamoth/Core/ExternalClient/ExternalClient.java
@@ -3,6 +3,8 @@
  */
 package Dynamoth.Core.ExternalClient;
 
+import Dynamoth.Core.Client.RPubClientId;
+import Dynamoth.Core.Manager.RPubManagerType;
 import Dynamoth.Core.RPubMessage;
 import Dynamoth.Core.RPubNetworkEngine;
 import Dynamoth.Core.RPubNetworkID;
@@ -10,10 +12,15 @@ import Dynamoth.Core.RPubPublishMessage;
 import Dynamoth.Core.RPubRawMessage;
 import Dynamoth.Mammoth.NetworkEngine.Exceptions.AlreadyConnectedException;
 import Dynamoth.Mammoth.NetworkEngine.Exceptions.NoSuchChannelException;
+import Dynamoth.Mammoth.NetworkEngine.Exceptions.NotConnectedException;
 import Dynamoth.Mammoth.NetworkEngine.NetworkEngineID;
 import Dynamoth.Mammoth.NetworkEngine.NetworkEngineListener;
 import Dynamoth.Util.Message.Reactor;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,6 +34,9 @@ public class ExternalClient {
 	private RPubNetworkEngine engine;
 	private String externalClientChannel = "externalclient";
 	
+	//private List<RPubNetworkID> registeredClients = new ArrayList<RPubNetworkID>();
+	private Map<Integer, RPubNetworkEngine> registeredClients = new HashMap<Integer, RPubNetworkEngine>();
+	
 	public ExternalClient() {
 	
 		
@@ -34,7 +44,8 @@ public class ExternalClient {
 	}
 	
 	public void initialize() {
-		// Connect to the network
+		// Initialize an engine for the External Client
+		// + one engine for each "connected" client
 		engine = new RPubNetworkEngine();
     	try {
 			engine.connect();
@@ -60,8 +71,6 @@ public class ExternalClient {
 			}
 		});
 		
-		// Create and register a reactor for RPub
-		//reactor = new Reactor("RClient" + this.id + "Reactor", engine);
 	}
 	
 	public void parseAndDispatch(String payload) {
@@ -93,18 +102,65 @@ public class ExternalClient {
 		}
 	}
 	
-	public void handleConnect(int clientId) {
+	public void handleConnect(final int clientId) {
+		// Register a new engine for that client
 		
+		RPubNetworkEngine eng = new RPubNetworkEngine(RPubManagerType.Dynamoth, false, new RPubNetworkID(clientId));
+		
+    	try {
+			eng.connect();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (AlreadyConnectedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// Subscribe to raw messages
+		eng.registerLowLevelListener(new RPubNetworkEngine.LowLevelListener() {
+			@Override
+			public void messageReceived(String channelName, RPubMessage message, int rawMessageSize) {
+				// Make sure this is a raw message and that the channel is appropriate
+				if (message instanceof RPubPublishMessage) {
+					RPubPublishMessage pm = (RPubPublishMessage)message;
+					
+					// Send to our external client
+					String topicName = externalClientChannel + "_" + clientId;
+					// Send raw message
+					RPubRawMessage raw = new RPubRawMessage(new RPubNetworkID(clientId), pm.getPayload().toString());
+					try {
+						engine.send(topicName, raw);
+					} catch (IOException ex) {
+						Logger.getLogger(ExternalClient.class.getName()).log(Level.SEVERE, null, ex);
+					} catch (NoSuchChannelException ex) {
+						Logger.getLogger(ExternalClient.class.getName()).log(Level.SEVERE, null, ex);
+					}
+				}
+			}
+		});
+		
+		registeredClients.put(clientId, eng);
 	}
 	
 	public void handleDisconnect(int clientId) {
-	
+		// Disconnect engine
+		RPubNetworkEngine eng = registeredClients.get(clientId);
+		try {
+			eng.disconnect();
+		} catch (IOException ex) {
+			Logger.getLogger(ExternalClient.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (NotConnectedException ex) {
+			Logger.getLogger(ExternalClient.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		registeredClients.remove(clientId);
 	}
 	
 	public void handlePublish(int clientId, String topic, String msg) {
-		RPubPublishMessage publish = new RPubPublishMessage(new RPubNetworkID(clientId), msg);
+		RPubNetworkEngine eng = registeredClients.get(clientId);
+		
 		try {
-			engine.send(topic, publish);
+			eng.send(topic, msg);
 		} catch (IOException ex) {
 			Logger.getLogger(ExternalClient.class.getName()).log(Level.SEVERE, null, ex);
 		} catch (NoSuchChannelException ex) {
@@ -113,22 +169,23 @@ public class ExternalClient {
 	}
 	
 	public void handleSubscribe(int clientId, String topic) {
+		RPubNetworkEngine eng = registeredClients.get(clientId);
+		
 		try {
-			engine.subscribeChannel(topic, new RPubNetworkID(clientId));
+			eng.subscribeChannel(topic, new RPubNetworkID(clientId));
 		} catch (NoSuchChannelException ex) {
 			Logger.getLogger(ExternalClient.class.getName()).log(Level.SEVERE, null, ex);
 		}
 	}
 	
 	public void handleUnsubscribe(int clientId, String topic) {
+		RPubNetworkEngine eng = registeredClients.get(clientId);
+		
 		try {
-			engine.unsubscribeChannel(topic, new RPubNetworkID(clientId));
+			eng.unsubscribeChannel(topic, new RPubNetworkID(clientId));
 		} catch (NoSuchChannelException ex) {
 			Logger.getLogger(ExternalClient.class.getName()).log(Level.SEVERE, null, ex);
 		}
 	}
 	
-	public voide handlePublication(int clientId, String topic, String msg) {
-		
-	}
 }
